0eebaee476231d6ac9c1c1754da7d3af84fe54ef
amirkhan1092 <amirkhan1092@gmail.com>
2021-08-31 11:22:11 +0530

    power function with repition of recursion

---
+++ Algorithms/power_function.py
@@ -0,0 +1,30 @@
+"""
+the power function with min value of iteration 
+
+
+"""
+
+
+def pow(x:int, y:int)->int:
+    # s = 1
+    # for i in range(y):
+    #     s *= x
+    # return s
+    if y == 0: return 1
+    elif x == 0: return 0
+    else:
+        return x * pow(x, y - 1)
+        
+    # if y == 0:
+    #     return 1
+    # elif y % 2 == 0:
+    #     return pow(x, y // 2) * pow(x, y // 2)
+    # else:
+    #     return x * pow()
+    
+
+
+a = 2
+b = 5
+res = pow(a, b)
+print(f"The result of a to the power of b is {res}")
