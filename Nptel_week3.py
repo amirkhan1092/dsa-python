a775839a9b379232b34a0f0c3289b68d7d24ce80
amirkhan1092 <amirkhan1092@gmail.com>
2021-08-31 11:24:30 +0530

    nptel week 3

---
+++ Nptel_week3.py
@@ -0,0 +1,24 @@
+
+# nptell week 3 Assignment 
+
+"""
+Write a function expanding(l) that takes as input a list of integer l and returns True if the absolute 
+difference between each adjacent pair of elements strictly increases.
+
+>>> expanding([1,3,7,2,9])
+  True
+"""
+
+def expanding(l:list)->bool:
+    
+    if len(l) <= 2:
+        return True 
+    else:
+        ele1 = l[0]
+        ele2 = l[1]
+        diff = abs(ele1 - ele2)
+        return diff < (abs(l[1] - l[2])) and expanding(l[1:])     
+
+input_list = [1, 3, 7, 2, -6]
+out = expanding(input_list)
+print(f"{out}")
