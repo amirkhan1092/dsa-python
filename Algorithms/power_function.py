7248599cf25f88552a6484d57e186127978580be
amirkhan1092 <amirkhan1092@gmail.com>
2021-09-02 10:52:50 +0530

    power with recursion O(logn)

--- Algorithms/power_function.py
+++ Algorithms/power_function.py
@@ -10,10 +10,10 @@ def pow(x:int, y:int)->int:
     # for i in range(y):
     #     s *= x
     # return s
-    if y == 0: return 1
-    elif x == 0: return 0
-    else:
-        return x * pow(x, y - 1)
+    # if y == 0: return 1
+    # elif x == 0: return 0
+    # else:
+    #     return x * pow(x, y - 1)
         
     # if y == 0:
     #     return 1
@@ -21,7 +21,17 @@ def pow(x:int, y:int)->int:
     #     return pow(x, y // 2) * pow(x, y // 2)
     # else:
     #     return x * pow()
-    
+
+    temp = 0
+    if y == 0:
+        return 1
+
+    temp = pow(x, y//2)
+    if y % 2 == 0:
+        return temp * temp
+
+    else:
+        return x * temp * temp 
 
 
 a = 2
