9c0253f0304a693e8fcc1e97780259b6caf75556
amirkhan1092 <amirkhan1092@gmail.com>
2021-09-02 10:53:03 +0530

    compiler test

--- Algorithms/hello.c
+++ Algorithms/hello.c
@@ -1,9 +1,22 @@
 #include<stdio.h>
+#include<conio.h>
 
 int main()
 {
-    
-    printf("Hello World");
+    float b1, h1, b2, h2, value;
+    scanf("%f %f", &b1, &h1);
+    scanf("%f %f", &b2, &h2);
+    float t1, t2;
+    t1 = (b1*h1)/2;
+    t2 = (b2*h2)/2;
+   
+    if (t1 > t2)
+        printf("%.6f", t1);
+    else
+        printf("%.6f", t2);
+
+
+
     return 0;
 
 }
