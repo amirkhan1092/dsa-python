f0269c6c2b7fe4a362058d8e03c518c444f0e18c
amirkhan1092 <amirkhan1092@gmail.com>
2021-09-16 09:56:46 +0530

    csv update

--- python practical/csv_file_update.py
+++ python practical/csv_file_update.py
@@ -3,18 +3,26 @@ create a csv file by entering user-id and password, read and search the password
 
 """
 
+# import pandas as pd
+# data = []
+# flag = 'Y'
+# while flag != 'N':
+#     user_id = input("Enter the user-id: ")
+#     password = input("Enter the password: ")
+#     data.append([user_id, password])
+#     flag = input("Do you want to enter another entry Y/N: ").strip()
+
+
+
+# df = pd.DataFrame(data, columns=["user-id", "password"])
+# print(df)
+# df.to_csv('e:\\Python39\\dsa-python\\dsa-python\\dsa-python\\python practical\\password.csv', index=False)
+
+
+# read and search password by user-id 
 import pandas as pd
+user_id = input("Enter the user-id for password: ")
 
-data = []
-flag = 'Y'
-while flag != 'N':
-    user_id = input("Enter the user-id: ")
-    password = input("Enter the password: ")
-    data.append([user_id, password])
-    flag = input("Do you want to enter another entry Y/N: ").strip()
-
-
-
-df = pd.DataFrame(data, columns=["user-d", "password"])
+df = pd.read_csv("e:\\Python39\\dsa-python\\dsa-python\\dsa-python\\python practical\\password.csv")
 print(df)
-df.to_csv('e:\\Python39\\dsa-python\\dsa-python\\dsa-python\\python practical\\password.csv')
+# print(f"The password at user-id: {user_id} is {df[user_id]}")
